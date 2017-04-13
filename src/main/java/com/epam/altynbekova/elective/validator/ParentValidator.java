bd7339177928c53272ff130bba0e152760644371
VERS <gulzhanaltynbekova@gmail.com>
2017-04-13 09:38:02 +0600

    Make ParentValidator abstract

--- src/main/java/com/epam/altynbekova/elective/validator/ParentValidator.java
+++ src/main/java/com/epam/altynbekova/elective/validator/ParentValidator.java
@@ -1,15 +1,9 @@
 package com.epam.altynbekova.elective.validator;
 
-public class ParentValidator implements Validator {
+public abstract class ParentValidator implements Validator {
     private String message;
 
     ParentValidator() {
-    }
-
-
-    @Override
-    public Boolean isValid(String parameter) {
-        return null;
     }
 
     public String getMessage() {
